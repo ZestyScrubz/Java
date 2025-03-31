b0327b62e98dff6ef4ac1a93959c54de2c8ccfb2
Zusty. <isaachj06@gmail.com>
2025-03-31 18:22:34 -0400

    Assignemnt 4

---
+++ Assignment 4/FileSystemObject.java
@@ -0,0 +1,18 @@
+
+import java.nio.file.FileSystem;
+
+
+
+public class FileSystemObject implements Comparable<Object> {
+    private String name;
+    private OrderedListADT<FileSystemObject> children;
+    private FileSystemObject parent;
+    private int id;  
+
+    public FileSystemObject(String name, int id) {
+        this.name = name;
+        this.id = id;
+
+        if ()
+    }
+}
